387a92016f4021e1de9f41dcb81b755dca9ed372
t-tamaru <t-tamaru@sakura.ad.jp>
2020-09-01 22:51:23 +0900

    受講の流れまでcss実装

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // タブレットの時はPC版縮小表示
-  let ua = navigator.userAgent;
-  if((ua.indexOf('iPhone') > 0) || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)){
-      jQuery('head').prepend('<meta name="viewport" content="width=device-width,initial-scale=1">');
-  } else {
-      jQuery('head').prepend('<meta name="viewport" content="width=1440">');
-  }
+let ua = navigator.userAgent;
+if((ua.indexOf('iPhone') > 0) || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)){
+    jQuery('head').prepend('<meta name="viewport" content="width=device-width,initial-scale=1">');
+} else {
+    jQuery('head').prepend('<meta name="viewport" content="width=1440">');
+}
+
+$(function () {
+    $('.loopslider__images').clone().appendTo('.loopslider__wrapper');
+});
